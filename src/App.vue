b0323190d1fd5624cd9083c0d0158e0837a165fb
wdfaESfaef <2622389575@qq.com>
2025-10-05 02:23:45 +0800

    Counter

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { useCounterStore } from './stores/counter';
+  const store = useCounterStore()
+
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +10,8 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div>Counter: {{ store.count }}</div>
+  <button @click="store.increment()">Increment</button>
 </template>
 
 <style scoped></style>
